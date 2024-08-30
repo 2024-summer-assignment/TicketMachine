254c13b27d8b068a18ba1040a1174dd6deeb875d
Tomaki.s <s.tomaki.sys23@morijyobi.ac.jp>
2024-08-30 10:11:02 +0900

    sp4の実装 close #24, close #25

--- src/Cart.java
+++ src/Cart.java
@@ -27,63 +27,34 @@ import java.io.*;
     }
 
 
-
     public void addItem(Item item) {
 
         //同じ商品がすでにカートにあるかチェックする。
-
         for(CartItem cartItem :cartItems) {
 
             if(cartItem.getId() == item.getId()) {
-
                 //ある場合
-
                 //quantityを+1する。
-
                 cartItem.setQuantity(cartItem.getQuantity() + 1);
-
                 return;
-
             }
-
         }
-
-
 
         //ない場合
 
         this.cartItems.add(new CartItem(item.getId(), item.getName(), item.getPrice()));
-
     }
-
-
 
     public List<CartItem> getCartItems() {
-
         return this.cartItems;
-
     }
 
-    
 
     public int getTotalPrice() {
-
         int total = 0;
-
-
-
         for(CartItem cartItem :cartItems) {
-
             total += cartItem.getPrice() * cartItem.getQuantity();
-
         }
-
-
-
         return total;
-
     }
-
-
-
 }
